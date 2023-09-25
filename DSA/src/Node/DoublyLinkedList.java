81706ecf76d74e4ab61f2158e21401de87bb818c
nguyenthinhhung203 <130766266+nguyenthinhhung203@users.noreply.github.com>
2023-09-25 17:23:37 +0700

    Add files via upload

---
+++ DSA/src/Node/DoublyLinkedList.java
@@ -0,0 +1,142 @@
+package Node;
+
+public class DoublyLinkedList {
+    private Node head;
+    private int size;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (size == 0) {
+            head = newNode;
+        } else {
+            // Co it nhat 1 phan tu
+            newNode.setNext(head);
+            head.setPrevious(newNode);
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addLast(int data) {
+        if (size == 0) {
+            addFirst(data);
+        } else {
+            // Bước 1: Tạo node mới
+            Node newNode = new Node(data);
+            // Bước 2: Tìm ra node cuối??
+            Node lastNode = head;
+            while (lastNode.getNext() != null) {
+                lastNode = lastNode.getNext();
+            }
+            // Buoc 3: set next node cuoi -> new node
+            lastNode.setNext(newNode);
+            newNode.setPrevious(lastNode);
+            // Buoc 4: Tang size len 1 don vi
+            size++;
+        }
+    }
+
+    public void add(int data, int position) {
+        if (position == 0) {
+            addFirst(data);
+        } else if (position == size) {
+            addLast(data);
+        } else if (position < 0 || position > size) {
+            throw new RuntimeException("Không hợp lệ!");
+        } else {
+            // Bước 1: Cần tìm node vị trị position -1
+            Node previousNode = head;
+            for (int i = 0; i < position - 1; i++) {
+                previousNode = previousNode.getNext();
+            }
+            //
+            Node currentNode = previousNode.getNext(); // position
+
+            // Bước 2: Tạo node mới từ dữ liệu truyền vào
+            Node newNode = new Node(data);
+
+            // Buoc 3: tao lien ket
+            newNode.setPrevious(previousNode);
+            previousNode.setNext(newNode);
+
+            newNode.setNext(currentNode);
+            currentNode.setPrevious(newNode);
+            // Buoc 4: Tang size
+            size++;
+        }
+    }
+
+
+    public void display() {
+        if (size == 0) {
+            System.out.println("Danh sach lien ket rong!");
+            return;
+        }
+        Node tmp = head;
+        while (tmp != null) {
+            System.out.print(tmp + "<=>");
+            tmp = tmp.getNext();
+        }
+        System.out.print("NULL\n");
+    }
+
+
+    public void deleteFirst() {
+        if (size == 0) {
+            System.out.println("Danh sách đã rỗng");
+            return;
+        }
+
+        if (size == 1) {
+            head = null;
+        } else {
+            head = head.getNext();
+            head.setPrevious(null);
+        }
+        size--;
+    }
+
+    public void deleteLast() {
+        if (size == 0) {
+            System.out.println("Danh sách đã rỗng");
+            return;
+        }
+
+        if (size == 1) {
+            head = null;
+        } else {
+            Node lastNode = head;
+            while (lastNode.getNext() != null) {
+                lastNode = lastNode.getNext();
+            }
+            lastNode.getPrevious().setNext(null);
+        }
+        size--;
+    }
+
+    public void deleteAt(int position) {
+        if (position < 0 || position >= size) {
+            throw new RuntimeException("Vị trí không hợp lệ");
+        }
+
+        if (position == 0) {
+            deleteFirst();
+        } else if (position == size - 1) {
+            deleteLast();
+        } else {
+            Node currentNode = head;
+            for (int i = 0; i < position; i++) {
+                currentNode = currentNode.getNext();
+            }
+
+            currentNode.getPrevious().setNext(currentNode.getNext());
+            currentNode.getNext().setPrevious(currentNode.getPrevious());
+            size--;
+        }
+    }
+}
---
+++ DSA/src/Node/Main.java
@@ -0,0 +1,31 @@
+package Node;
+
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList linkedList = new DoublyLinkedList();
+        linkedList.addFirst(10);
+        linkedList.display();
+        linkedList.addFirst(20);
+        linkedList.display();
+        linkedList.addLast(30);
+        linkedList.display(); // 20 10 30
+        linkedList.addLast(40);
+        linkedList.display(); // 20 10 30 40
+        linkedList.add(50, 1); // 20 50 10 30 40
+        linkedList.display();
+        linkedList.add(60, 0);
+        linkedList.display();
+        linkedList.add(80, 6);
+        linkedList.display();
+
+        linkedList.deleteFirst();
+        linkedList.display();
+
+        linkedList.deleteLast();
+        linkedList.display();
+
+        linkedList.deleteAt(2);
+        linkedList.display();
+
+    }
+}
